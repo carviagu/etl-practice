07903c0f14917578c9e9e83c16c11f52ad3a0001
carviagu <carlos.vinals@gmail.com>
2021-10-26 16:21:17 +0000

    Update 04b-extract_xlsx.R

--- code/04b-extract_xlsx.R
+++ code/04b-extract_xlsx.R
@@ -1,7 +1,11 @@
 library(readxl)
-path_to_file <- "../data/ejemplos_lectura.xlsx"
+
+# setwd("~/etl") # para mi máquina local
+
+path_to_file <- "data/ejemplos_lectura.xlsx"
 df_ejemplos <- read_xlsx(path_to_file)
 df_ejemplos
+# aún estando oculta puede leer la primera hoja
 
 # Hay hojas ocultas
 excel_sheets(path_to_file)
@@ -10,7 +14,14 @@ excel_sheets(path_to_file)
 df_ejemplos <- read_xlsx(path_to_file, sheet = "Fechas")
 df_ejemplos
 
+# Le decimos que lea a partir de la tercera fila
+df_fechas <- read_xlsx(path_to_file, 
+                       sheet = "Fechas", 
+                       skip = 2)
+df_fechas
+
 # Para leer regiones
+# c("skip", "skip", "skip", "guess", "guess", "guess", "guess")
 df_fechas <- read_xlsx(path_to_file, 
                        sheet = "Fechas", 
                        skip = 2, 
